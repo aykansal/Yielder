da7246d36889593b395f8d88c8641e507433badc
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2025-08-28 22:46:05 +0530

    estimate24hvolume working fine now

---
+++ processes/check.js
@@ -0,0 +1,84 @@
+//   - First time seeing pool. Initializing history structure for 3TFMtVjYssqCuNPIqcIKn-tM4bWq5KtQ9NKweWxvV1w
+HISTORICAL_RESERVES = { latest_reserve: [], history: [] };
+poolDataHistory = {
+  latest_reserve: {
+    timestamp: 1756394477,
+    reserves: {
+      _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+      Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+    },
+  },
+  history: [
+    {
+      timestamp: 1756394477,
+      reserves: {
+        _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+        Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+      },
+    },
+  ],
+};
+today = { year_month_day: 19700121, day: 21, month: 1, year: 1970 };
+yesterday = { year_month_day: 19700121, day: 21, month: 1, year: 1970 };
+yesterdays_last_entry_0 = null;
+latest_yesterday_timestamp_0 = 0;
+yesterdays_last_entry_1 = {
+  timestamp: 1756394477,
+  reserves: {
+    _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+    Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+  },
+};
+latest_yesterday_timestamp_1 = 1756394477;
+yesterdays_last_entry_2 = {
+  timestamp: 1756394477,
+  reserves: {
+    _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+    Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+  },
+};
+latest_yesterday_timestamp_2 = 1756394477;
+yesterdays_last_entry_3 = {
+  timestamp: 1756394477,
+  reserves: {
+    _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+    Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+  },
+};
+latest_yesterday_timestamp_3 = 1756394477;
+filtered_history = [
+  {
+    timestamp: 1756394477,
+    reserves: {
+      _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+      Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+    },
+  },
+];
+past_data_was_found = false;
+poolDataHistory.history = [
+  {
+    timestamp: 1756394477,
+    reserves: {
+      _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+      Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+    },
+  },
+];
+poolDataHistory.history = [
+  {
+    timestamp: 1756394477,
+    reserves: {
+      _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+      Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+    },
+  },
+];
+yesterdays_last_entry = {
+  timestamp: 1756394477,
+  reserves: {
+    _IxG5qxfgSBBj1wH7BL0j1vkihOcfx2ntXS19NZjDFU: 100,
+    Zg8ihIkD2Tpm2E0vRbJSD0J3Jb3dqK8XUZ4OlOZ9kcc: 200,
+  },
+};
+0.0;
